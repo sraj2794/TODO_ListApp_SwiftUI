eb768286f034018bd04cc17961ae4570b26699b2
Raj Shekhar <sraj2794@gmail.com>
2023-02-06 00:25:41 +0530

    Setup Project Done

---
+++ ToDO_iOSApp/ToDO_iOSApp/ToDO_iOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDO_iOSAppApp.swift
+//  ToDO_iOSApp
+//
+//  Created by Raj Shekhar on 06/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDO_iOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
